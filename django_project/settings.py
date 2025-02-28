d81971007eccc1d60a8e4be7e715fd00c816085a
luisHidalgo-git <hidalgovazquezluisangel2004@gmail.com>
2025-02-27 22:41:16 -0600

    postgres 1.5.2

--- django_project/settings.py
+++ django_project/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.getenv('DATABASE_PUBLIC_URL'))
 }
 
 
