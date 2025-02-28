d9d71a944cf5e822d49f1cb6090535f5fe4d52e1
luisHidalgo-git <hidalgovazquezluisangel2004@gmail.com>
2025-02-27 18:14:07 -0600

    modificacion en settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static')]
+
+ALLOW_HOST = [
+    'web-production-95e21.up.railway.app',
+    'localhost'
+]
