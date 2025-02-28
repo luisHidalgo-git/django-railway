6b75bfce4da08d7d87b44ae7b60a053bf76d280b
luisHidalgo-git <hidalgovazquezluisangel2004@gmail.com>
2025-02-28 00:00:02 -0600

    rest framework

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('blog.urls')),
-    path('', include('home.urls'))
+    path('', include('home.urls')),
+    path("api/", include('tasks.urls'))
 ]
