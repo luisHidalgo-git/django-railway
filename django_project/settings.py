2d5c77a00407d9a6576ef3ee6738c201d22c9006
luisHidalgo-git <hidalgovazquezluisangel2004@gmail.com>
2025-02-28 17:53:23 -0600

    postgres 1.5.5

--- django_project/settings.py
+++ django_project/settings.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
     'blog',
-    'rest_framework',
-    'tasks',
+    #'rest_framework',
+    #'tasks',
     'home'
 ]
 
